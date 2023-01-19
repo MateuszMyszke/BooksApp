493169315b1ea24d330441464f257ff7dcfefd68
Mateusz Myszke <myszke93@gmail.com>
2023-01-19 15:24:03 +0100

    Add initActions function

--- src/js/script.js
+++ src/js/script.js
@@ -9,14 +9,22 @@
     containerOf: {
       booksList: '.books-list',
     },
+
+    book: {
+      image: '.books-list .book__image',
+    },
   };
+
+  const classFav = {
+    favorite: 'favorite',
+  };
+  
   const templates = {
-    books: Handlebars.compile(
-      document.querySelector(select.templateOf.book).innerHTML
-    ),
+    books: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
   };
 
   const render = function () {
+    
     for (const book of dataSource.books){
       const generatedHTML = templates.books(book);
       const generateDOMElement = utils.createDOMFromHTML(generatedHTML); 
@@ -25,5 +33,21 @@
     }
   };
 
+  const favoriteBooks = [];
+
+  function initActions() {
+    const bookList = document.querySelectorAll(select.book.image);
+
+    for (let book of bookList) {
+      book.addEventListener('dblclick', function (event) {
+        event.preventDefault();
+        book.classList.add(classFav.favorite);
+        const bookId = book.getAttribute('data-id');
+        favoriteBooks.push(bookId);
+      });
+    }
+  }
+
   render();
+  initActions();
 }
