1c35f84884583891d5924dbdd8e781923fdbd76b
Mateusz Myszke <myszke93@gmail.com>
2023-01-19 15:19:47 +0100

    Add render function

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+{
+  ('use strict');
+
+  const select = {
+    templateOf: {
+      book: '#template-book',
+    },
+
+    containerOf: {
+      booksList: '.books-list',
+    },
+  };
+  const templates = {
+    books: Handlebars.compile(
+      document.querySelector(select.templateOf.book).innerHTML
+    ),
+  };
+
+  const render = function () {
+    for (const book of dataSource.books){
+      const generatedHTML = templates.books(book);
+      const generateDOMElement = utils.createDOMFromHTML(generatedHTML); 
+      const booksContainer = document.querySelector(select.containerOf.booksList);
+      booksContainer.appendChild(generateDOMElement);
+    }
+  };
+
+  render();
+}
